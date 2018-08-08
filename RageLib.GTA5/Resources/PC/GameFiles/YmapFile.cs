7415ba0b2d6f537096da0405c5c9b1d0ef496896
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2018-08-08 14:18:25 +0200

    Cleanup + add YmapFile accessors

--- RageLib.GTA5/Resources/PC/GameFiles/YmapFile.cs
+++ RageLib.GTA5/Resources/PC/GameFiles/YmapFile.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using RageLib.Resources.GTA5.PC.Meta;
 using SharpDX;
 
@@ -15,6 +16,19 @@ namespace RageLib.Resources.GTA5.PC.GameFiles
         public Vector3 StreamingExtentsMax { get => this.CMapData.StreamingExtentsMax; set => this.CMapData.StreamingExtentsMax = value; }
         public Vector3 EntitiesExtentsMin { get => this.CMapData.EntitiesExtentsMin; set => this.CMapData.EntitiesExtentsMin = value; }
         public Vector3 EntitiesExtentsMax { get => this.CMapData.EntitiesExtentsMax; set => this.CMapData.EntitiesExtentsMax = value; }
+
+        public List<Meta.ExtendedTypes.CEntityDef> Entities { get => this.CMapData.Entities; set => this.CMapData.Entities = value; }
+
+        public Array_Structure ContainerLods { get => CMapData.ContainerLods; set => CMapData.ContainerLods = value; }
+        public List<Meta.ExtendedTypes.Unk_975711773> BoxOccluders { get => CMapData.BoxOccluders; set => CMapData.BoxOccluders = value; }
+        public List<Meta.ExtendedTypes.Unk_2741784237> OccludeModels { get => CMapData.OccludeModels; set => CMapData.OccludeModels = value; }
+        public Array_uint PhysicsDictionaries { get => CMapData.PhysicsDictionaries; set => CMapData.PhysicsDictionaries = value; }
+        public Meta.ExtendedTypes.rage__fwInstancedMapData InstancedData { get => CMapData.InstancedData; set => CMapData.InstancedData = value; }
+        public List<Meta.ExtendedTypes.CTimeCycleModifier> TimeCycleModifiers { get => CMapData.TimeCycleModifiers; set => CMapData.TimeCycleModifiers = value; }
+        public List<Meta.ExtendedTypes.CCarGen> CarGenerators { get => CMapData.CarGenerators; set => CMapData.CarGenerators = value; }
+        public Meta.ExtendedTypes.CLODLight LODLightsSOA { get => CMapData.LODLightsSOA; set => CMapData.LODLightsSOA = value; }
+        public Meta.ExtendedTypes.CDistantLODLight DistantLODLightsSOA { get => CMapData.DistantLODLightsSOA; set => CMapData.DistantLODLightsSOA = value; }
+        public Meta.ExtendedTypes.CBlockDesc Block { get => CMapData.Block; set => CMapData.Block = value; }
 
         public override void Parse()
         {
@@ -38,21 +52,6 @@ namespace RageLib.Resources.GTA5.PC.GameFiles
             this.CMapData.Build(mb, true);
 
             ResourceFile.ResourceData = this.CMapData.Meta;
-        }
-
-        public void AddStructureAndEnumInfo(MetaBuilder mb, MetaName name)
-        {
-            var ei = MetaInfo.GetStructureEnumInfo(name);
-            var sci = MetaInfo.GetStructureChildInfo(name);
-            var sei = MetaInfo.GetStructureEnumInfo(name);
-
-            for (int i = 0; i < sei.Length; i++)
-                mb.AddEnumInfo((MetaName) sei[i].EnumNameHash);
-
-            mb.AddStructureInfo(name);
-
-            for(int i=0; i<sci.Length; i++)
-                AddStructureAndEnumInfo(mb, (MetaName) sci[i].StructureNameHash);
         }
 
     }
