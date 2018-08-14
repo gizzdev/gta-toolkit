640bdc1a4c3de652d46a70f4790f6568203b4e43
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2018-08-14 17:08:29 +0200

    YdrFile : Remove accessors

--- RageLib.GTA5/Resources/PC/GameFiles/YdrFile.cs
+++ RageLib.GTA5/Resources/PC/GameFiles/YdrFile.cs
@@ -29,30 +29,7 @@ namespace RageLib.Resources.GTA5.PC.GameFiles
 {
     public class YdrFile : GameFileBase_Resource<Drawables.Drawable>
     {
-        Drawables.Drawable Drawable;
-
-        public ulong ShaderGroupPointer { get => Drawable.ShaderGroupPointer; set => Drawable.ShaderGroupPointer = value; }
-        public ulong SkeletonPointer { get => Drawable.SkeletonPointer; set => Drawable.SkeletonPointer = value; }
-        public Vector3 BoundingCenter { get => (Vector3) Drawable.BoundingCenter; set => Drawable.BoundingCenter = (RAGE_Vector3) value; }
-        public float BoundingSphereRadius { get => Drawable.BoundingSphereRadius; set => Drawable.BoundingSphereRadius = value; }
-        public Vector4 BoundingBoxMin { get => (Vector4) Drawable.BoundingBoxMin; set => Drawable.BoundingBoxMin = (RAGE_Vector4) value; }
-        public Vector4 BoundingBoxMax { get => (Vector4) Drawable.BoundingBoxMax; set => Drawable.BoundingBoxMax = (RAGE_Vector4) value; }
-        public ulong DrawableModelsHighPointer { get => Drawable.DrawableModelsHighPointer; set => Drawable.DrawableModelsHighPointer = value; }
-        public ulong DrawableModelsMediumPointer { get => Drawable.DrawableModelsMediumPointer; set => Drawable.DrawableModelsMediumPointer = value; }
-        public ulong DrawableModelsLowPointer { get => Drawable.DrawableModelsLowPointer; set => Drawable.DrawableModelsLowPointer = value; }
-        public ulong DrawableModelsVeryLowPointer { get => Drawable.DrawableModelsVeryLowPointer; set => Drawable.DrawableModelsVeryLowPointer = value; }
-        public float LodGroupHigh { get => Drawable.LodGroupHigh; set => Drawable.LodGroupHigh = value; }
-        public float LodGroupMed { get => Drawable.LodGroupMed; set => Drawable.LodGroupMed = value; }
-        public float LodGroupLow { get => Drawable.LodGroupLow; set => Drawable.LodGroupLow = value; }
-        public float LodGroupVlow { get => Drawable.LodGroupVlow; set => Drawable.LodGroupVlow = value; }
-        public uint Unknown_80h { get => Drawable.Unknown_80h; set => Drawable.Unknown_80h = value; }
-        public uint Unknown_84h { get => Drawable.Unknown_84h; set => Drawable.Unknown_84h = value; }
-        public uint Unknown_88h { get => Drawable.Unknown_88h; set => Drawable.Unknown_88h = value; }
-        public uint Unknown_8Ch { get => Drawable.Unknown_8Ch; set => Drawable.Unknown_8Ch = value; }
-        public ulong JointsPointer { get => Drawable.JointsPointer; set => Drawable.JointsPointer = value; }
-        public uint Unknown_98h { get => Drawable.Unknown_98h; set => Drawable.Unknown_98h = value; }
-        public uint Unknown_9Ch { get => Drawable.Unknown_9Ch; set => Drawable.Unknown_9Ch = value; }
-        public ulong DrawableModelsXPointer { get => Drawable.DrawableModelsXPointer; set => Drawable.DrawableModelsXPointer = value; }
+        public Drawables.Drawable Drawable;
 
         public YdrFile()
         {
