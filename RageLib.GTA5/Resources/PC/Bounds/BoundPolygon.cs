16a84be6859738e9cd309eb8c99ac4c719062343
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2018-10-29 17:56:04 +0100

    Implement some BoundPolygons

--- RageLib.GTA5/Resources/PC/Bounds/BoundPolygon.cs
+++ RageLib.GTA5/Resources/PC/Bounds/BoundPolygon.cs
@@ -20,6 +20,8 @@
     THE SOFTWARE.
 */
 
+using System;
+
 namespace RageLib.Resources.GTA5.PC.Bounds
 {
     public class BoundPolygon : ResourceSystemBlock
@@ -27,12 +29,10 @@ namespace RageLib.Resources.GTA5.PC.Bounds
         public override long Length => 0x10;
 
         // structure data
-        public uint Unknown_0h;
-        public ushort Unknown_4h;
-        public ushort Unknown_6h;
-        public ushort Unknown_8h;
-        public ushort Unknown_Ah;
-        public uint Unknown_Ch;
+        public byte[] data = new byte[0x10];
+
+        private BoundPolygonBase _Polygon;
+        public BoundPolygonBase Polygon { get { return this.GetPolygon(); } }
 
         /// <summary>
         /// Reads the data-block from a stream.
@@ -40,12 +40,7 @@ namespace RageLib.Resources.GTA5.PC.Bounds
         public override void Read(ResourceDataReader reader, params object[] parameters)
         {
             // read structure data
-            this.Unknown_0h = reader.ReadUInt32();
-            this.Unknown_4h = reader.ReadUInt16();
-            this.Unknown_6h = reader.ReadUInt16();
-            this.Unknown_8h = reader.ReadUInt16();
-            this.Unknown_Ah = reader.ReadUInt16();
-            this.Unknown_Ch = reader.ReadUInt32();
+            this.data = reader.ReadBytes(0x10);
         }
 
         /// <summary>
@@ -54,12 +49,209 @@ namespace RageLib.Resources.GTA5.PC.Bounds
         public override void Write(ResourceDataWriter writer, params object[] parameters)
         {
             // write structure data
-            writer.Write(this.Unknown_0h);
-            writer.Write(this.Unknown_4h);
-            writer.Write(this.Unknown_6h);
-            writer.Write(this.Unknown_8h);
-            writer.Write(this.Unknown_Ah);
-            writer.Write(this.Unknown_Ch);
+            writer.Write(this.data);
+        }
+
+        public BoundPolygonBase GetPolygon()
+        {
+            byte b0 = this.data[0];
+
+            BoundPolygonType type = (BoundPolygonType)(b0 & 7);
+
+            BoundPolygonBase p = this._Polygon;
+
+            if(p == null)
+            {
+                switch (type)
+                {
+                    case BoundPolygonType.Triangle: p = new BoundPolygonTriangle(); break;
+                    case BoundPolygonType.Sphere: p = new BoundPolygonSphere(); break;
+                    case BoundPolygonType.Capsule: p = new BoundPolygonCapsule(); break;
+                    case BoundPolygonType.Box: p = new BoundPolygonBox(); break;
+                    case BoundPolygonType.Cylinder: p = new BoundPolygonCylinder(); break;
+                    default: break;
+                }
+            }
+
+            if (p != null)
+            {
+                p.Read(this.data, 0);
+            }
+
+            return p;
+        }
+    }
+
+    public enum BoundPolygonType
+    {
+        Triangle = 0,
+        Sphere = 1,
+        Capsule = 2,
+        Box = 3,
+        Cylinder = 4,
+    }
+
+    public abstract class BoundPolygonBase
+    {
+        public BoundPolygonType Type { get; set; }
+        public abstract void Read(byte[] bytes, int offset);
+
+        public override string ToString()
+        {
+            return Type.ToString();
+        }
+    }
+
+    public class BoundPolygonTriangle : BoundPolygonBase
+    {
+        public float TriArea;
+        public ushort TriIndex1;
+        public ushort TriIndex2;
+        public ushort TriIndex3;
+        public short EdgeIndex1;
+        public short EdgeIndex2;
+        public short EdgeIndex3;
+
+        public int VertIndex1 { get { return TriIndex1 & 0x7FFF; } }
+        public int VertIndex2 { get { return TriIndex2 & 0x7FFF; } }
+        public int VertIndex3 { get { return TriIndex3 & 0x7FFF; } }
+        public bool VertFlag1 { get { return (TriIndex1 & 0x8000) > 0; } }
+        public bool VertFlag2 { get { return (TriIndex2 & 0x8000) > 0; } }
+        public bool VertFlag3 { get { return (TriIndex3 & 0x8000) > 0; } }
+
+
+        public BoundPolygonTriangle()
+        {
+            Type = BoundPolygonType.Triangle;
+        }
+        public override void Read(byte[] bytes, int offset)
+        {
+            TriArea = BitConverter.ToSingle(bytes, offset + 0);
+            TriIndex1 = BitConverter.ToUInt16(bytes, offset + 4);
+            TriIndex2 = BitConverter.ToUInt16(bytes, offset + 6);
+            TriIndex3 = BitConverter.ToUInt16(bytes, offset + 8);
+            EdgeIndex1 = BitConverter.ToInt16(bytes, offset + 10);
+            EdgeIndex2 = BitConverter.ToInt16(bytes, offset + 12);
+            EdgeIndex3 = BitConverter.ToInt16(bytes, offset + 14);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + ": " + VertIndex1.ToString() + ", " + VertIndex2.ToString() + ", " + VertIndex3.ToString();
+        }
+    }
+
+    public class BoundPolygonSphere : BoundPolygonBase
+    {
+        public ushort SphereType;
+        public ushort SphereIndex;
+        public float SphereRadius;
+        public uint Unused0;
+        public uint Unused1;
+
+        public BoundPolygonSphere()
+        {
+            Type = BoundPolygonType.Sphere;
+        }
+        public override void Read(byte[] bytes, int offset)
+        {
+            SphereType = BitConverter.ToUInt16(bytes, offset + 0);
+            SphereIndex = BitConverter.ToUInt16(bytes, offset + 2);
+            SphereRadius = BitConverter.ToSingle(bytes, offset + 4);
+            Unused0 = BitConverter.ToUInt32(bytes, offset + 8);
+            Unused1 = BitConverter.ToUInt32(bytes, offset + 12);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + ": " + SphereIndex.ToString() + ", " + SphereRadius.ToString();
+        }
+    }
+
+    public class BoundPolygonCapsule : BoundPolygonBase
+    {
+        public ushort CapsuleType;
+        public ushort CapsuleIndex1;
+        public float CapsuleRadius;
+        public ushort CapsuleIndex2;
+        public ushort Unused0;
+        public uint Unused1;
+
+        public BoundPolygonCapsule()
+        {
+            Type = BoundPolygonType.Capsule;
+        }
+        public override void Read(byte[] bytes, int offset)
+        {
+            CapsuleType = BitConverter.ToUInt16(bytes, offset + 0);
+            CapsuleIndex1 = BitConverter.ToUInt16(bytes, offset + 2);
+            CapsuleRadius = BitConverter.ToSingle(bytes, offset + 4);
+            CapsuleIndex2 = BitConverter.ToUInt16(bytes, offset + 8);
+            Unused0 = BitConverter.ToUInt16(bytes, offset + 10);
+            Unused1 = BitConverter.ToUInt32(bytes, offset + 12);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + ": " + CapsuleIndex1.ToString() + ", " + CapsuleIndex2.ToString() + ", " + CapsuleRadius.ToString();
+        }
+    }
+
+    public class BoundPolygonBox : BoundPolygonBase
+    {
+        public uint BoxType;
+        public short BoxIndex1;
+        public short BoxIndex2;
+        public short BoxIndex3;
+        public short BoxIndex4;
+        public uint Unused0;
+
+        public BoundPolygonBox()
+        {
+            Type = BoundPolygonType.Box;
+        }
+        public override void Read(byte[] bytes, int offset)
+        {
+            BoxType = BitConverter.ToUInt32(bytes, offset + 0);
+            BoxIndex1 = BitConverter.ToInt16(bytes, offset + 4);
+            BoxIndex2 = BitConverter.ToInt16(bytes, offset + 6);
+            BoxIndex3 = BitConverter.ToInt16(bytes, offset + 8);
+            BoxIndex4 = BitConverter.ToInt16(bytes, offset + 10);
+            Unused0 = BitConverter.ToUInt32(bytes, offset + 12);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + ": " + BoxIndex1.ToString() + ", " + BoxIndex2.ToString() + ", " + BoxIndex3.ToString() + ", " + BoxIndex4.ToString();
+        }
+    }
+
+    public class BoundPolygonCylinder : BoundPolygonBase
+    {
+        public ushort CylinderType;
+        public ushort CylinderIndex1;
+        public float CylinderRadius;
+        public ushort CylinderIndex2;
+        public ushort Unused0;
+        public uint Unused1;
+
+        public BoundPolygonCylinder()
+        {
+            Type = BoundPolygonType.Cylinder;
+        }
+        public override void Read(byte[] bytes, int offset)
+        {
+            CylinderType = BitConverter.ToUInt16(bytes, offset + 0);
+            CylinderIndex1 = BitConverter.ToUInt16(bytes, offset + 2);
+            CylinderRadius = BitConverter.ToSingle(bytes, offset + 4);
+            CylinderIndex2 = BitConverter.ToUInt16(bytes, offset + 8);
+            Unused0 = BitConverter.ToUInt16(bytes, offset + 10);
+            Unused1 = BitConverter.ToUInt32(bytes, offset + 12);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + ": " + CylinderIndex1.ToString() + ", " + CylinderIndex2.ToString() + ", " + CylinderRadius.ToString();
         }
     }
 }
