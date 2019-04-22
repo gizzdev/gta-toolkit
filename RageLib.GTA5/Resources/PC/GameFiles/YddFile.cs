e15ef05fd947ec970a17c240742ce27c1dd90bd5
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2019-04-22 17:24:20 +0200

    Add YddFile

---
+++ RageLib.GTA5/Resources/PC/GameFiles/YddFile.cs
@@ -0,0 +1,53 @@
+﻿/*
+    Copyright(c) 2016 Neodymium
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+
+using System;
+using System.Collections.Generic;
+using RageLib.GTA5.Resources.PC;
+using RageLib.Resources.GTA5.PC.Meta;
+using SharpDX;
+
+namespace RageLib.Resources.GTA5.PC.GameFiles
+{
+    public class YddFile : GameFileBase_Resource<Drawables.GtaDrawableDictionary>
+    {
+        public Drawables.GtaDrawableDictionary DrawableDictionary;
+
+        public YddFile()
+        {
+            
+        }
+
+        public override void Parse()
+        {
+            this.DrawableDictionary = this.ResourceFile.ResourceData;
+        }
+
+        public override void Build()
+        {
+            this.ResourceFile.Version = ResourceFileTypes_GTA5_pc.DrawableDictionary.Version;
+            this.ResourceFile.ResourceData = this.DrawableDictionary;
+        }
+
+    }
+
+}
