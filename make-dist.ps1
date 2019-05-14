156cc667ab68d70b09109ce6d19d8827a3e60976
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2019-05-14 13:18:25 +0200

    Add make-dist.ps1

---
+++ make-dist.ps1
@@ -0,0 +1,41 @@
+7z a -tzip gta-toolkit-0.0.0-debug.zip `
+  .\keys\*.dat `
+  .\RageLib\bin\Debug\*.dll `
+  .\RageLib\bin\Debug\*.exe `
+
+7z a -tzip gta-toolkit-0.0.0-debug.zip `
+  .\RageLib.GTA5\bin\Debug\*.dll `
+  .\RageLib.GTA5\bin\Debug\*.exe `
+
+7z a -tzip gta-toolkit-0.0.0-debug.zip `
+  .\Tools\ArchiveTool\bin\Debug\*.dll `
+  .\Tools\ArchiveTool\bin\Debug\*.exe `
+
+7z a -tzip gta-toolkit-0.0.0-debug.zip `
+  .\Tools\MetaTool\bin\Debug\*.dll `
+  .\Tools\MetaTool\bin\Debug\*.exe `
+
+7z a -tzip gta-toolkit-0.0.0-debug.zip `
+  .\Tools\TextureTool\bin\Debug\*.dll `
+  .\Tools\TextureTool\bin\Debug\*.exe
+
+7z a -tzip gta-toolkit-0.0.0-release.zip `
+  .\keys\*.dat `
+  .\RageLib\bin\Release\*.dll `
+  .\RageLib\bin\Release\*.exe `
+
+7z a -tzip gta-toolkit-0.0.0-release.zip `
+  .\RageLib.GTA5\bin\Release\*.dll `
+  .\RageLib.GTA5\bin\Release\*.exe `
+
+7z a -tzip gta-toolkit-0.0.0-release.zip `
+  .\Tools\ArchiveTool\bin\Release\*.dll `
+  .\Tools\ArchiveTool\bin\Release\*.exe `
+
+7z a -tzip gta-toolkit-0.0.0-release.zip `
+  .\Tools\MetaTool\bin\Release\*.dll `
+  .\Tools\MetaTool\bin\Release\*.exe `
+  
+7z a -tzip gta-toolkit-0.0.0-release.zip `
+  .\Tools\TextureTool\bin\Release\*.dll `
+  .\Tools\TextureTool\bin\Release\*.exe
